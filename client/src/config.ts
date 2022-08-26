809eff7cde92c12c5c8321a266a753514d6a9fc1
G-oracle <akinlolugbemisola@gmail.com>
2022-08-26 19:44:42 +0100

    Please enter the commit message for your changes. Lines starting

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'wh222rndli';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-livvp81k.us.auth0.com',
+  clientId: 'jwIkxKs6ha2bBJx7aAUJ2WfAFytf1n6l',
+  callbackUrl: 'http://localhost:3000/callback'
+};
